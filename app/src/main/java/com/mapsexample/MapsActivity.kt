1b87397eab233f3d70e13c23d83015b287232177
Laura <practicante1@giro.com.mx>
2020-02-12 18:55:19 -0600

    Initial commit

---
+++ app/src/main/java/com/mapsexample/MapsActivity.kt
@@ -0,0 +1,105 @@
+package com.mapsexample
+
+import android.content.pm.PackageManager
+import android.graphics.Color
+import android.location.Location
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.*
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback,GoogleMap.OnMarkerClickListener {
+
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
+    private lateinit var lastLocatioon: Location
+
+    companion object{
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1
+    }
+
+    private lateinit var map: GoogleMap
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_maps)
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this@MapsActivity)
+    }
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+
+
+
+
+
+        map.setOnMarkerClickListener(this)
+        map.uiSettings.isZoomControlsEnabled = true
+
+        setUpMap()
+
+    }
+
+    private fun placeMarker(location: LatLng){
+
+        val markerOption = MarkerOptions().position(location)
+        markerOption.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_VIOLET))
+        map.addMarker(markerOption)
+
+    }
+
+    private fun setUpMap(){
+
+        if (ActivityCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,
+                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERMISSION_REQUEST_CODE)
+            return
+
+        }
+
+        map.isMyLocationEnabled = true
+        map.mapType = GoogleMap.MAP_TYPE_NORMAL
+
+        fusedLocationClient.lastLocation.addOnSuccessListener(this) {location ->
+            if (location != null){
+                lastLocatioon = location
+                val currentLatLong = LatLng(location.latitude,location.longitude)
+                placeMarker(currentLatLong)
+                map.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLong,13f))
+
+                map.addCircle(CircleOptions()
+                    .center(currentLatLong)
+                    .radius(300.00)
+                    .strokeWidth(3f)
+                    .strokeColor(Color.BLUE)
+                    .fillColor(Color.GRAY)
+                )
+            }
+
+        }
+
+    }
+
+    override fun onMarkerClick(p0: Marker?) = false
+}
